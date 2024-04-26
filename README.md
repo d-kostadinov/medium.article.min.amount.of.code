c2f3873c59344c390c1b416090636bb6737673de
d-kostadinov <dobri.kostadinov@gmail.com>
2024-04-26 10:45:31 +0300

    working version
    working version

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # medium.article.min.amount.of.code
 This is a demo codebase used for this medium article:
-test
---
+++ app/src/main/java/com/medium/d/kostadinov/repo1/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.medium.d.kostadinov.repo1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.medium.d.kostadinov.repo1.ui.theme.MediumMinAmountofLinesTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.kotlinx.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.lazy.items
+import io.ktor.client.call.body
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.utils.EmptyContent.contentType
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MediumMinAmountofLinesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PostList()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun PostList(postsViewModel: PostsViewModel = viewModel()) {
+        val posts by postsViewModel.posts.collectAsState(initial = emptyList())
+
+        LazyColumn {
+            items(posts) { post ->
+                Text("Title: ${post.title}")
+            }
+        }
+    }
+
+    @Serializable
+    data class Post(val userId: Int, val id: Int, val title: String, val body: String)
+
+    class PostsViewModel : ViewModel() {
+        private val client = HttpClient(Android) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        private val _posts = MutableStateFlow<List<Post>>(emptyList())
+        val posts: StateFlow<List<Post>> = _posts
+
+        init {
+            viewModelScope.launch {
+                _posts.value = fetchPosts()
+            }
+        }
+
+        private suspend fun fetchPosts(): List<Post> {
+            return client.get("https://jsonplaceholder.typicode.com/posts").body()
+        }
+    }
+}
